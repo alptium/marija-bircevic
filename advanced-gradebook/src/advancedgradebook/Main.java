39914ce41d73c29089a3cfd57c771a9db9157f62
MarijaBircevic <marija.bircevic@gmail.com>
2018-03-04 21:08:30 +0100

    Created Advanced Gradebook Project

--- advanced-gradebook/src/advancedgradebook/Main.java
+++ advanced-gradebook/src/advancedgradebook/Main.java
@@ -5,39 +5,32 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		try(Scanner user_input = new Scanner(System.in)) {
-		
-		
-		int gradeSum = 0;
-		
-	
-		while (true)
-		{
-			
-			String studentFirstName;
-			System.out.print("Enter students First name ");
-			studentFirstName = user_input.next();
-		
-			String studentLastName;
-			System.out.print("Enter " + studentFirstName + "'s" + " Last Name ");
-			studentLastName = user_input.next();
-		
-			System.out.print("Enter " + studentFirstName + " " + studentLastName + " grade ");
-			int studentGrade = user_input.nextInt();
 
-			gradeSum += studentGrade;
-			
-			System.out.print("Do you  wish to enter new student? (Y/N) ");
-			String answer = user_input.next();
-	
-			if (answer.equals("N"))	
-				break;		
-		}
-		
+		try (Scanner userInput = new Scanner(System.in)) {
+
+			int gradeSum = 0;
+
+			while (true) {
+
+				System.out.print("Enter students First name ");
+				String studentFirstName = userInput.next();
+
+				System.out.print("Enter " + studentFirstName + "'s" + " Last Name ");
+				String studentLastName = userInput.next();
+
+				System.out.print("Enter " + studentFirstName + " " + studentLastName + " grade ");
+				int studentGrade = userInput.nextInt();
+
+				gradeSum += studentGrade;
+
+				System.out.print("Do you  wish to enter new student? (Y/N) ");
+				String answer = userInput.next();
+
+				if (answer.equals("N"))
+					break;
+			}
+
 			System.out.print("Grade sum: " + gradeSum);
 		}
-	}	
+	}
 }
-		
